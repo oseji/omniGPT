d9843f03df9cad202740c2836966ff6aba6a6614
oseji <jiade1233@gmail.com>
2023-05-17 00:58:12 +0100

    added section 3 and 4

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,12 +6,17 @@ module.exports = {
       colors: {
         backgroundMain: "#111826",
         section1Btn: " #1FC77E",
+        headingAlt: "#1FC77E",
       },
       height: {
         110: "500px",
+        device: "90vh",
       },
       fontSize: {
         "7xl": "5rem",
+      },
+      width: {
+        "4/10": "40%",
       },
     },
   },
